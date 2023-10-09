9f7fac2fb1e005e1812091f071a1dfda1c4c99a0
Mélian Fruchart <melian.fruchart@grenoble-inp.org>
2023-10-09 21:24:41 +0200

    docs: added function documentation and translated comments in english

--- projet_periode1/rainbow_create.c
+++ projet_periode1/rainbow_create.c
@@ -3,67 +3,72 @@
 #include <string.h>
 #include "hash.h"
 
+/// @brief Reduction function used to create a rainbow table.
+/// @param hash hash to be used to determine the next password
+/// @param call reduction variation to prevent the same hash to get the same password
+/// @param word string that will contain the next password.
 void reduction(pwhash hash, int call, char *word) {
     for(int i=0; i<M; i++) {
         word[i] = (char) ((hash/(i+1) + call)%26) + 'a';
     }
-    printf("%lu -> %s ->", hash, word);
+    // printf("%lu -> %s ->", hash, word);
 }
 
-void create(FILE * in_file, FILE * out_file){
+/// @brief Writes N pass0 passL tuples in a file, which corresponds to a N size rainbow table.
+/// @param in_file file containing oridinal input passwords. If NULL, randomly generates those passwords.
+/// @param out_file file where to write the tuples.
+void create(FILE * in_file, FILE * out_file) {
     char * pass0;
     char * passL;
 
-    // ajout des N couples de mots de passe au fichier
+    // adding the N tuples to the file
     for(int i = 0; i < N; i++) {
-        // allocation de deux chaines de même longueur que le mot de passe
+        // string allocation of the size of the password's length
         pass0 = (char*) malloc(sizeof(char) * M);
         passL = (char*) malloc(sizeof(char) * M);
 
-        // si un fichier où lire les password pass0 n'est pas donné, alors générer ces nombres aléatoirement
-        // sinon les tirer du fichier
+        // if an input file is not provided, then randomly generates the input passwords "pass0"
+        // else use the ones from the file
         if(in_file==NULL) {
-            // génération aléatoire du mot de passe à M caractères
+            // random generation of M-long passowrd
             for(int j = 0; j < M; j++) {
                 pass0[j] = (rand() % 26) + 'a';
             }
-            printf("%s -> ",pass0);
+            // printf("%s -> ",pass0);
         }
         else {
             size_t len = 0;
             size_t read;
             char * line;
             read = getline(&line, &len, in_file);
-            // suppression du caractère de fin de ligne
+            // removing newline character
             strtok(line, "\n");
 
-            // copie dans pass0
-            // on ne peut utiliser pass0 directement à la place de line, car line n'a pas la taille souhaitée
+            // pass0 cannot be used directly instead of line, as the file reading does not have the right size
             strcpy(pass0, line);
 
             // fread(pass0, sizeof(char), N, in_file);
 
-            // si la longueur du mot de passe lu dans le fichier n'est celle attendue,
-            // alors fin du programme
+            // exit if the size of the password read from the file is not the right one
             if (strlen(pass0) != M) {
                printf("Le mot de passe '%s' lu dans le fichier donné n'a pas la bonne longueur (longueur = %ld, attendue = %d)\n", pass0, strlen(pass0), M);
             }
-            printf("%s -> ",pass0);
+            // printf("%s -> ",pass0);
         }
         
-        // premier hash en dehors de la boucle car on souhaite conserver la valeur de pass0
+        // first hash outside the main hash-reduction loop, to keep pass0's value in memory
         pwhash hashed = target_hash_function(pass0);
-        // premiere réduction pour avoir un nombre égal de hash et de réductions à faire dans la boucle
+        // first reduction to have the same number of hash and reduction operations in the loop
         reduction(hashed, 0, passL);
 
-        // boucle de hash et de réduction
+        // hash-reduction loop, generating an L-chain
         for(int k=1; k<L; k++) {
             hashed = target_hash_function(passL);
             reduction(hashed, k, passL);
         }
-        printf("\n\n");
+        // printf("\n\n");
 
-        // écriture du couple pass0 passL dans le fichier de sortie
+        // writing pass0 passL tuple in the output file
         fprintf(out_file, "%s %s\n", pass0, passL);
 
         free(pass0);
@@ -78,27 +83,27 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // si un fichier d'entrée des mots de passe de départ est donné, alors lecture de ce fichier
+    // if an input file is provided, then reads it
     FILE *input_file = NULL;
     if (argc == R+2) {
         input_file = fopen(argv[R+1], "r");
     }
 
-    // écriture pour chaque fichier passé en paramètre
+    // computing and writing N "pass0 passL" tuples into each file provided as parameter
     for(int r = 0; r < R; r++) {
         FILE * rfile = fopen(argv[r+1], "w");
         if(rfile == NULL) {
             printf("Error opening file %s", argv[r-1]);
         }
-        // écriture dans le fichier des couples pass0, passL
+        // computing and writing the tuples
         create(input_file, rfile);
         fclose(rfile);  
     }
     
-    // fermeture du fichier d'entrée s'il est donné
+    // closing of the input file if provided
     if (input_file) fclose(input_file);
     
-    // test de valeur
+    // value testing
     pwhash mot = target_hash_function("testing");
     return 0;
 }
