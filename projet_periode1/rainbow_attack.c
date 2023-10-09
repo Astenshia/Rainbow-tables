bd4b8bcd5802d50592463445cc0b82645f276211
Mélian Fruchart <melian.fruchart@grenoble-inp.org>
2023-10-09 22:20:16 +0200

    feat: added parameters' number check in rainbow_attack.c

--- projet_periode1/rainbow_attack.c
+++ projet_periode1/rainbow_attack.c
@@ -1,3 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "hash.h"
+
+int main(int argc, char const *argv[])
+{
+    // checking the number of arguments
+    if (argc < R + 3){
+        printf("Usage : <file1>.....<file R><file R+1 = attacked><file R+2 = results>\n\n");
+        exit(EXIT_FAILURE);
+    }
+
+    
+    return 0;
+}
