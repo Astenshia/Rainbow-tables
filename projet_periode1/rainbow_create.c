4a4b111e6cb0eb24ea22124d048f1735af41e4f1
Melian Fruchart <fruchame@ensipc111.ensimag.fr>
2023-10-09 13:58:01 +0200

    Ajout de commentaires et modification de la fonction reduction pour utiliser un pointeur.
    On peut désormais utiliser n'importe quelle taille de mot de passe.

--- projet_periode1/rainbow_create.c
+++ projet_periode1/rainbow_create.c
@@ -3,34 +3,47 @@
 #include <string.h>
 #include "hash.h"
 
-char * reduction(pwhash hash, int call) {
-    char * word = malloc(sizeof(char) * M);
+void reduction(pwhash hash, int call, char *word) {
     for(int i=0; i<M; i++) {
         word[i] = (char) ((hash/(i+1) + call)%26) + 'a';
     }
     printf("%lu -> %s ->", hash, word);
-    return word;
 }
 
 void create(FILE * in_file, FILE * out_file){
     char * pass0;
     char * passL;
     for(int i = 0; i < N; i++) {
+        // si un fichier où lire les password pass0 n'est pas donné, alors générer ces nombres aléatoirements
+        // sinon les tirer du fichier
         if(in_file==NULL) {
-            pass0 = malloc(sizeof(char) * M);
+            // allocation de deux chaine de même longueur que le mot de passe
+            pass0 = (char*) malloc(sizeof(char) * M);
+            passL = (char*) malloc(sizeof(char) * M);
+
+            // génération aléatoire du mot de passe à M caractères
             for(int j = 0; j < M; j++) {
                 pass0[j] = (rand() % 26) + 'a';
             }
             printf("%s -> ",pass0);
+            
+            // premier hash en dehors de la boucle car on souhaite conserver la valeur de pass0
             pwhash hashed = target_hash_function(pass0);
-            char * word = reduction(hashed, 0); //jusque la ca marche
+            // premiere réduction en dehors de la boucle
+            reduction(hashed, 0, passL);
+
+            // boucle de hash et de réduciton
             for(int k=1; k<L; k++) {
-                hashed = target_hash_function(word);
-                word = reduction(hashed, k);
+                hashed = target_hash_function(passL);
+                reduction(hashed, k, passL);
             }
-            passL=word;
             printf("\n\n");
+
+            // écriture du couple pass0 passL dans le fichier de sortie
             fprintf(out_file, "%s %s\n", pass0, passL);
+
+            free(pass0);
+            free(passL);
         }
         else {
             size_t len = 0;
@@ -49,22 +62,27 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
-    FILE * input_file;
+    // si un fichier d'entrée des pass0 est donné, alors lecture de ce fichier
+    FILE *input_file = NULL;
     if (argc == R+2) {
         input_file = fopen(argv[R+1], "r");
     }
-    else {
-        input_file = NULL;
-    }
+
+    // écriture pour chaque fichier passé en paramètre
     for(int r = 0; r < R; r++) {
         FILE * rfile = fopen(argv[r+1], "w");
         if(rfile == NULL) {
             printf("Error opening file %s", argv[r-1]);
         }
+        // écriture dans le fichier des couples pass0, passL
         create(input_file, rfile);
         fclose(rfile);  
     }
-    fclose(input_file);
-    pwhash mot = target_hash_function("proute");
+    
+    // fermeture du fichier d'entrée s'il est donné
+    if (input_file) fclose(input_file);
+    
+    // test de valeur
+    pwhash mot = target_hash_function("testing");
     return 0;
 }
