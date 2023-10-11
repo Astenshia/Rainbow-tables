d5226b7d04f624e7a0a42270a01c7371c92662af
Mélian Fruchart <melian.fruchart@grenoble-inp.org>
2023-10-11 17:26:11 +0200

    feat: debugging rainbow_attack

--- projet_periode1/rainbow_attack.c
+++ projet_periode1/rainbow_attack.c
@@ -3,22 +3,35 @@
 #include "reduce.h"
 
 int passL_in_file(char * pass, FILE * r_file) {
-    int nc, read;
+    printf("pif.1\n");
+
+    int nc;
     char * line, * passL;
+    size_t len = 0;
+    size_t read;
+    printf("pif.2\n");
+    if (r_file == NULL) printf("NULL\n");
     // read lines in R file
-    while (read = getline(&line, NULL, r_file)) {
+    while (read = getline(&line, &len, r_file)) {
         // Count number of chars until whitespace
+        printf("pif.3\n");
         nc = 1;
         while(fgetc(r_file) != ' ') {
             nc += 1;
         }
+        printf("pif.4\n");
         // Compare passL and pass
         passL = line + nc;
         if(strcmp(pass, passL) == 0) {
             printf("FOUND !\n");
             return 1;
         }
+        printf("pif.5\n");
     }
+
+    printf("pif - fin\n");
+
+    if (line) free(line);
     return 0;
 }
 
@@ -39,23 +52,31 @@ int main(int argc, char const *argv[])
 
     input_file = fopen(argv[R+1], "r");
     output_file = fopen(argv[R+2], "r");
-
+    printf("a\n");
     while(read = getline(&line, &len, input_file)) {
+        printf("b\n");
         hash = atoi(line);
         reduce(hash, call, pass);
+        printf("c\n");
         // Do L times hash and reduction
         for(int l=0; l<L; l++) {
             // Get new word from hash
             // Test if word in files
 
-            for(int r=0; r<R; r++) {
+            printf("d\n");
+            for(int r=1; r<R; r++) {
+                printf("d.1\n");
                 r_file = fopen(argv[r], "w");
+                printf("d.2\n");
                 found = passL_in_file(pass, r_file);
+                printf("d.3\n");
                 if(found) {
                     break;
                 }
+                printf("d.4\n");
                 fclose(r_file);
             }
+            printf("e\n");
             if(found) {
                 printf("%lu hash gives %s pass\n", hash, pass);
                 fprintf(output_file, "%s\n", pass);
@@ -64,11 +85,14 @@ int main(int argc, char const *argv[])
             else {
                 printf("%lu hash not in files, compute next pass\n", hash);
             }
+            printf("f\n");
             // Current pass not in R files, compute next pass
             call += 1;
             hash = target_hash_function(pass);
             reduce(hash, call, pass);
+            printf("g\n");
         }
+        printf("h\n");
         // Pass not found after L reductions
         if(!found) {
             printf("%s hash does not give any correct pass after L reductions\n", line);
