9397d4bf34a0629ecf95354c41f1ed1fb8bbd438
Melian Fruchart <fruchame@ensipc77.ensimag.fr>
2023-10-13 10:18:09 +0200

    feat: remove commented code in reduce.h

--- projet_periode1/reduce.h
+++ projet_periode1/reduce.h
@@ -13,16 +13,10 @@
 /// @param word string that will contain the next password.
 void reduce(pwhash hash, int call, char *word) {
     unsigned long long modulo = (hash + call) % ((unsigned long long) pow(26, M));
-    // if (call == L-1) printf("%ld\n", modulo);
-
+    
     for (int i = M-1; i >= 0 ; i--)
     {
         word[i] = modulo / (unsigned long long) pow(26, i) + 'a';
         modulo = modulo % (unsigned long long) pow(26, i);
     }
-
-    // for(int i=0; i<M; i++) {
-    //     word[i] = (char) ((hash/(i+1) + call)%26) + 'a';
-    // }
-    // printf("%lu -> %s ->", hash, word);
 }
